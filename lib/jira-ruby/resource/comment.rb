fcb5c59462d60c457c8c761549d988094160701a
James Muir <muir.29@osu.edu>
2012-03-11 20:18:29 -0400

    added has_one associations for author, updatedAuthor on Comments

--- lib/jira-ruby/resource/comment.rb
+++ lib/jira-ruby/resource/comment.rb
@@ -7,6 +7,11 @@ module JIRA
     class Comment < JIRA::Base
       belongs_to :issue
 
+      has_one :author,  :class => JIRA::Resource::User
+      has_one :update_author, :class => JIRA::Resource::User,
+              :attribute_key => "updateAuthor"
+
+
       nested_collections true
     end
 
